fdf4631fa61c0d4622cba10922fcc5c1a0b3f6c3
binaudd <binaudd@K129-8.dep-info.iutmontp.univ-montp2.fr>
2021-09-24 13:13:50 +0200

    modif ex12 - Array

--- TP/TP1/ArrayFormat.java
+++ TP/TP1/ArrayFormat.java
@@ -1,20 +1,13 @@
+import java.util.Arrays;
 
 public class ArrayFormat{
 	public static void main(String[] args) {
 		int[] tab = new int[100];
 
-		for (int i = 0; i < 20; i++) {
-			tab[i] = 0;
-		}
+		Arrays.fill(tab, 0, 20, 0);
+		Arrays.fill(tab, 20, 80, 6);
+		Arrays.fill(tab, 80, 100, 8);
 
-		for (i = 20; i < 80; i++) {
-			tab[i] = 2;
-		}
-
-		for (i = 80; i < 100; i++) {
-			tab[i] = 8;
-		}
-
-		//System.out.println(tab.toString());
+		System.out.println(Arrays.toString(tab));
 	}	
 }
