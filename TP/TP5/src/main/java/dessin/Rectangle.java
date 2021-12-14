49ee78f6e0e57300d8c556bcbf18506b25f4b011
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-14 11:26:00 +0100

    TP5 - Fin Ex3

--- TP/TP5/src/main/java/dessin/Rectangle.java
+++ TP/TP5/src/main/java/dessin/Rectangle.java
@@ -1,19 +1,44 @@
 package dessin;
 
+import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class Rectangle extends ObjetGraphique{
 
     private Rectangle2D rectangle;
 
+    public Rectangle() {
+        super();
+        this.rectangle = Rectangle2D.EMPTY;
+    }
+
+    public Rectangle(Color c) {
+        super(c);
+        this.rectangle = Rectangle2D.EMPTY;
+    }
+
+    public Rectangle(int x, int y, int largeur, int hauteur){
+        super();
+        this.rectangle = new Rectangle2D(x, y, largeur, hauteur);
+    }
+    public Rectangle(Point2D p, int largeur, int hauteur){
+        super();
+        this.rectangle = new Rectangle2D(p.getX(), p.getY(), largeur, hauteur);
+    }
+    public Rectangle(Point2D p, int largeur, int hauteur, Color c){
+        super(c);
+        this.rectangle = new Rectangle2D(p.getX(), p.getY(), largeur, hauteur);
+    }
+
     @Override
     public void dessineToi(GraphicsContext context) {
-
+        context.fillRect(rectangle.getMinX(), rectangle.getMinY(), rectangle.getWidth(), rectangle.getHeight());
     }
 
     @Override
     public boolean contient(int x, int y) {
-        return false;
+        return rectangle.contains(x, y);
     }
 }
