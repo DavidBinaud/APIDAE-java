085ff613460f0a9af8a757a9d076c0aafb17c6a1
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-14 11:42:27 +0100

    TP5 - Fin Ex4

---
+++ TP/TP5/src/main/java/dessin/Cercle.java
@@ -0,0 +1,33 @@
+package dessin;
+
+import javafx.geometry.Point2D;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+
+public class Cercle extends ObjetGraphique{
+
+    private Point2D centre;
+
+    private int rayon;
+
+    public Cercle(Color couleur, Point2D centre, int rayon) {
+        super(couleur);
+        this.centre = centre;
+        this.rayon = rayon;
+    }
+
+    public Cercle(Point2D centre, int rayon) {
+        this.centre = centre;
+        this.rayon = rayon;
+    }
+
+    @Override
+    public void dessineToi(GraphicsContext context) {
+        context.fillOval(centre.getX() - rayon, centre.getY() - rayon, rayon, rayon);
+    }
+
+    @Override
+    public boolean contient(int x, int y) {
+        return Math.pow(x - centre.getX(), 2) + Math.pow(y - centre.getY(), 2) <= Math.pow(rayon, 2);
+    }
+}
