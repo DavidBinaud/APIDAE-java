58ce45075eaf80df189ff0ffc1a4c560beb87072
DavidBinaud <david.binaud@etu.umontpellier.fr>
2021-12-14 12:36:50 +0100

    TP5 - Exo 7+8

--- TP/TP5/src/main/java/toto/Toto.java
+++ TP/TP5/src/main/java/toto/Toto.java
@@ -3,8 +3,13 @@ package toto;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -14,15 +19,23 @@ public class Toto extends Application {
     public void start(Stage stage) throws Exception {
         Button btn = new Button();
         btn.setText("Fermer");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        btn.setOnAction(new EventHandler<>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 System.exit(0);
             }
         });
+        TotoJPanel panel = new TotoJPanel();
+        Canvas canvas = new Canvas(350,350);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        panel.paintComponent(gc);
+
+        panel.setCenter(canvas);
+        panel.setBottom(btn);
+        panel.setAlignment(btn, Pos.CENTER);
 
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        root.getChildren().add(panel);
 
         Scene scene = new Scene(root, 400, 400);
 
--- TP/TP5/src/main/java/toto/TotoJPanel.java
+++ TP/TP5/src/main/java/toto/TotoJPanel.java
@@ -1,28 +1,33 @@
 package toto;
 
 import dessin.Cercle;
+import dessin.ObjetGraphique;
 import dessin.Rectangle;
 import javafx.geometry.Point2D;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.BorderPane;
 
+import java.util.ArrayList;
+
 public class TotoJPanel extends BorderPane {
+
+    private ArrayList<ObjetGraphique> objetGraphiques;
 
     public TotoJPanel() {
         super();
-        Canvas canvas = new Canvas(400,400);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        Cercle cercle1 = new Cercle(new Point2D(200, 200), 100);
-        Cercle cercle2 = new Cercle(new Point2D(160, 150), 20);
-        Cercle cercle3 = new Cercle(new Point2D(240, 150), 20);
-        Rectangle rectangle = new Rectangle(150, 220, 100, 40);
-
-        cercle1.dessineToi(gc);
-        cercle2.dessineToi(gc);
-        cercle3.dessineToi(gc);
-        rectangle.dessineToi(gc);
-        this.setCenter(canvas);
+        objetGraphiques = new ArrayList<>();
+        objetGraphiques.add(new Cercle(new Point2D(200, 200), 100));
+        objetGraphiques.add(new Cercle(new Point2D(160, 150), 20));
+        objetGraphiques.add(new Cercle(new Point2D(240, 150), 20));
+        objetGraphiques.add(new Rectangle(150, 220, 100, 40));
     }
+
+    public void paintComponent(GraphicsContext gc){
+        for (ObjetGraphique objetGraphique: objetGraphiques) {
+            objetGraphique.dessineToi(gc);
+        }
+    }
+
+
 }
